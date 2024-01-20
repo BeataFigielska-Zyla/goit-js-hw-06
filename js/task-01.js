2a509bac43c76226a391023a8c5df3ffae8ced56
BeataFigielska-Zyla <beata.figielska@gmail.com>
2024-01-20 12:08:11 +0100

    task1

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+// zadanie 1 Policzy i wypisze w konsoli liczbę kategorii w ul#categories, czyli elementy li.item.
+    const categoriesList = document.querySelectorAll('#categories .item');
+
+    
+    const numberOfCategories = categoriesList.length;
+    console.log(`Number of categories: ${numberOfCategories}\n`);
+
+// zadanie 2 Dla każdego elementu li.item na liście ul#categories, znajdzie i wypisze w konsoli tekst nagłówka elementu (tag <h2>) i liczbę elementów w danej kategorii (wszystkich w jej obrębie <li>).
+
+    categoriesList.forEach(category => {
+      const categoryName = category.querySelector('h2').textContent;
+      const elementsCount = category.querySelectorAll('ul li').length;
+
+      console.log(`Category: ${categoryName}`);
+      console.log(`Elements: ${elementsCount}\n`);
+    });
