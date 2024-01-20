7e6fdca6f5cdf561c70b3260a1174697628a959b
BeataFigielska-Zyla <beata.figielska@gmail.com>
2024-01-20 20:24:24 +0100

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Napisz skrypt, który dla każdego elementu tablicy ingredients:
+// Utworzy oddzielny element < li >.Obowiązkowo użyj metody document.createElement().
+// Doda nazwę elementu (string z tablicy) jako jego zawartość tekstową.
+// Doda do elementu klasę item.
+// Następnie jedna operacja umieści wszystkie < li > na liście ul#ingredients.
+
+
+    const ingredientsList = document.getElementById('ingredients');
+
+    
+    ingredients.forEach(ingredient => {
+     
+      const listItem = document.createElement('li');
+
+     
+      listItem.textContent = ingredient;
+
+      
+      listItem.classList.add('item');
+
+      
+      ingredientsList.appendChild(listItem);
+    });
