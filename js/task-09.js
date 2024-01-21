2a69a3353a05d06744d16be6c5b6b66a387bd2d4
BeataFigielska-Zyla <beata.figielska@gmail.com>
2024-01-21 14:27:17 +0100

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Napisz skrypt, który zmienia kolor tła elementu <body> (poprzez style inline) po kliknięciu na button.change-color i wyświetla wartość koloru w span.color.
+
+// Aby wygenerować losowy kolor użyj funkcji getRandomHexColor.
+
+
+
+const widget = document.querySelector('.widget');
+ 
+const colorSpan = widget.querySelector('.color');
+    
+const changeColorButton = widget.querySelector('.change-color');
+    
+
+changeColorButton.addEventListener('click', function () {
+    
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+
+  colorSpan.textContent = randomColor;
+  
+    });
